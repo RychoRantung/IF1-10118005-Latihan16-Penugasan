814fbc7735f4abc08e1b50d981cde4a088b4419e
Rycho Rantung <55700409+RychoRantung@users.noreply.github.com>
2019-09-26 13:45:43 +0700

    Add files via upload

---
+++ IF1-10118005-Latihan16-Penugasan/src/if1/pkg10118005/latihan16/penugasan/IF110118005Latihan16Penugasan.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118005.latihan16.penugasan;
+
+/**
+ *
+ * @author USER
+  * NAMA   : Rycho Rantung
+ * KELAS  : IF-1
+ * NIM    : 10118005
+ * Deskripsi Program : Program ini berisi program yang menampilkan penugasan
+ */
+public class IF110118005Latihan16Penugasan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 0;
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Nilai c = " + c+"\n");
+        
+        c = a + b;
+        System.out.println("c = a + b = " + c);
+        c += a;
+        System.out.println("c += a = " + c);
+        c -= a;
+        System.out.println("c -= a = " + c);
+        c *= a;
+        System.out.println("c *= a = " + c);
+        a = 10;
+        c = 15;
+        c /= a;
+        System.out.println("c /= a = " + c);
+    
+        a = 10;
+        c = 15;
+        c %= a;
+        System.out.println("c %= a = " + c);
+        c <<= a;
+        System.out.println("c <<= a = " + c);
+        c >>= a;
+        System.out.println("c >>= a = " + c);
+        c &= a;
+        System.out.println("c &= a = " + c);
+        c ^= a;
+        System.out.println("c ^= a = " + c);
+        c |= a;
+        System.out.println("c |= a = " + c);
+                
+        
+        
+        
+        
+        
+        
+    }
+    
+}
